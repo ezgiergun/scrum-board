fd74535199e525b59c9451512ed8f8a8048fd497
ezgiergun <ezgiergun.k@gmail.com>
2025-06-03 14:10:29 +0300

    fix: remove eslint

--- knip.ts
+++ knip.ts
@@ -3,8 +3,6 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
   entry: [
     'app/**/*.{ts,tsx,js,jsx,mdx}',
-    'components/**/*.{ts,tsx,js,jsx}',
-    'lib/**/*.{ts,tsx,js,jsx}',
   ],
   project: ['**/*.{ts,tsx,js,jsx,mdx}'],
 };
