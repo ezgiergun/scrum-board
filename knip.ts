dee4706eea411ca5fc0850d01560aa0a70452c28
ezgiergun <ezgiergun.k@gmail.com>
2025-06-03 23:30:01 +0300

    fix: code format

--- knip.ts
+++ knip.ts
@@ -1,9 +1,7 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-  entry: [
-    'app/**/*.{ts,tsx,js,jsx,mdx}',
-  ],
+  entry: ['app/**/*.{ts,tsx,js,jsx,mdx}'],
   project: ['**/*.{ts,tsx,js,jsx,mdx}'],
 };
 
